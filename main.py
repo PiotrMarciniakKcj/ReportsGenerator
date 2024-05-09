aec56bd796d9af1af8d112b7b2aa59c39a64cd64
Dom <piotr.marciniak.kcj@gmail.com>
2024-05-09 05:40:14 +0200

    pasting tables works correctly

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import types
-
 import xlwings as xw
 import datetime as datetime
 from docxtpl import DocxTemplate
 
 
-# converts time from float to datetime
+# convert time from float to datetime
 def excel_time_to_datetime(excel_time):
     SECONDS_PER_DAY = 86400
     dt = datetime.datetime.utcfromtimestamp(excel_time * SECONDS_PER_DAY)
     return dt.strftime("%H:%M:%S")
 
 
-# returns formated table(without headers) from Excel as 2-dimensional array
+# return formated table(without headers) from Excel as 2-dimensional array
 def get_and_format_data(start_cell, end_cell, path):
     wb = xw.Book(path, read_only=True, )
     sheet = wb.sheets[0]
     table = sheet.range(start_cell, end_cell).value
     for row in table:
+        if row[0] == row[-1]:
+            continue
         if row != table[-1]:
             row[0] = excel_time_to_datetime(row[0])
             row[1] = excel_time_to_datetime(row[1])
@@ -26,48 +26,65 @@ def get_and_format_data(start_cell, end_cell, path):
         for x in range(2, len(row) - 1):
             row[x] = int(row[x])
         row[len(row) - 1] = ('{:.2f}'.format(round((row[len(row) - 1] * 100), 2)) + "%").replace('.', ',')
+    for row in table:
+        if row[0] == row[-1]:
+            table.remove(row)
     wb.close()
     return table
 
 
 # get YYYYMMDD date from the name of the Excel file
-def get_date_from_filename(filename):
-    date = filename[filename.find("_") + 1:filename.rfind("_", 0, filename.rfind("_", 0, filename.rfind("_")))]
+def get_date(filename):
+    #date = filename[filename.find("_") + 1:filename.rfind("_", 0, filename.rfind("_", 0, filename.rfind("_")))]
+    date = filename[filename.find("_") + 1:filename.rfind("_", 0, filename.rfind("_"))]
     return date
 
 
-# path = "C:\\Users\\User\Desktop\\102_20230516_090002_DR500_wClass.xlsx"
+# copy the contents of the tables from Excel and paste them into the word template
+def paste_tables(start_cell, end_cell, file, table):
+    for data_row in get_and_format_data(start_cell, end_cell, file):
+        table_row = table.add_row().cells
+        for x in range(0, len(data_row)):
+            table_row[x].text = str(data_row[x])
+
 
 # paths to Excel files
 DR_excel = "102_20230516_090002_DR500_wClass.xlsx"
 DP_excel = "102_20230516_140007_DP500_wClass.xlsx"
 N_excel = "102_20230515_210000_N200_wClass.xlsx"
 
-DR_date = get_date_from_filename(DR_excel)
-print(DR_date)
+# dates of the tests
+DR_date = get_date(DR_excel)
+DP_date = get_date(DP_excel)
+N_date = get_date(N_excel)
+
+# correct order of the tests
+order = [DR_date, DP_date, N_date]
+order.sort()
 
 # path to template file
 template = "template.docx"
 output = "output.docx"
-
-print(get_and_format_data('I2', 'N9', DR_excel))
-print(get_and_format_data('R2', 'X9', DR_excel))
 
 doc = DocxTemplate(template)
 doc.render({})
 
 tables = doc.tables
 
-DR_detection_table = tables[0]
-DR_identification_table = tables[1]
-DP_detection_table = tables[2]
-DP_identification_table = tables[3]
-N_detection_table = tables[4]
-N_identification_table = tables[5]
+# assigning correct table order
+DR_detection_table = tables[order.index(DR_date) * 2]
+DR_identification_table = tables[order.index(DR_date) * 2 + 1]
+DP_detection_table = tables[order.index(DP_date) * 2]
+DP_identification_table = tables[order.index(DP_date) * 2 + 1]
+N_detection_table = tables[order.index(N_date) * 2]
+N_identification_table = tables[order.index(N_date) * 2 + 1]
 
-for data_row in get_and_format_data('I2', 'N9', DR_excel):
-    table_row = DR_detection_table.add_row().cells
-    for x in range(0, len(data_row)):
-        table_row[x].text = str(data_row[x])
+# pasting the tables
+paste_tables('I2', 'N9', DR_excel, DR_detection_table)
+paste_tables('R2', 'X9', DR_excel, DR_identification_table)
+paste_tables('I2', 'N18', N_excel, N_detection_table)
+paste_tables('R2', 'X18', N_excel, N_identification_table)
+paste_tables('I2', 'N8', DP_excel, DP_detection_table)
+paste_tables('R2', 'X8', DP_excel, DP_identification_table)
 
 doc.save(output)
