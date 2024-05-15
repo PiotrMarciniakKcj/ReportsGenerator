01081f78fb1a513c83f711dff8440f339c7333a4
Dom <piotr.marciniak.kcj@gmail.com>
2024-05-15 15:33:11 +0200

    fixed formatting, subsections titles works correctly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import datetime as datetime
+import lxml
 from docx import Document
+from docx.shared import Cm
 from openpyxl import load_workbook
+from docx.enum.table import WD_ROW_HEIGHT_RULE, WD_TABLE_ALIGNMENT, WD_CELL_VERTICAL_ALIGNMENT
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import docx.oxml.ns as ns
 
 
 # convert float to string value with '%'
@@ -12,7 +17,7 @@ def float_to_percent(value):
 def enumerate_sheet(worksheet, max_val):
     for i, i_row in enumerate(worksheet):
         if i_row[max_val].value == "Suma:":
-            return str(i+1)
+            return str(i + 1)
 
 
 # return formatted tables from an Excel file
@@ -58,13 +63,48 @@ def get_date(filename):
 def paste_tables(formated_data, table_name, is_summary=False):
     for data_row in formated_data:
         if is_summary:
+            for x in range(0, 2):
+                table_name.rows[x].height_rule = WD_ROW_HEIGHT_RULE.EXACTLY
+                table_name.rows[x].height = Cm(0.7)
             row = table_name.rows[1].cells
             for x in range(0, 4):
                 row[x].text = str(data_row[x])
+                row[x].paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                row[x].vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER
         else:
-            table_row = table_name.add_row().cells
+            table_row = table_name.add_row()
+            table_row.height = Cm(0.5)
+            table_row.height_rule = WD_ROW_HEIGHT_RULE.EXACTLY
             for x in range(0, len(data_row)):
-                table_row[x].text = str(data_row[x])
+                table_row.cells[x].text = str(data_row[x])
+                table_row.cells[x].vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER
+                table_row.cells[x].paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    table_name.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+
+
+def format_document(document):
+    DR_text = 'Badanie w okresie przed południem (DR500)'
+    DP_text = 'Badanie w okresie po południu (DP500)'
+    N_text = 'Badanie w okresie nocnym (N200)'
+    if order[0] == DR_date:
+        text_list = [DR_text, DP_text, N_text]
+    elif order[0] == DP_date:
+        text_list = [DP_text, N_text, DR_text]
+    elif order[0] == N_date:
+        text_list = [N_text, DR_text, DP_text]
+    print(order)
+    for paragraph in document.paragraphs:
+        if 'order1' in paragraph.text:
+            paragraph.text = text_list[0]
+        if 'order2' in paragraph.text:
+            paragraph.text = text_list[1]
+        if 'order3' in paragraph.text:
+            paragraph.text = text_list[2]
+
+
+
+
 
 
 # paths to Excel files
@@ -90,12 +130,12 @@ doc = Document(template)
 tables = doc.tables
 
 # assigning correct table order
-DR_detection_table = tables[(order.index(DR_date) * 2)-8]
-DR_identification_table = tables[(order.index(DR_date) * 2 + 1)-8]
-DP_detection_table = tables[(order.index(DP_date) * 2)-8]
-DP_identification_table = tables[(order.index(DP_date) * 2 + 1)-8]
-N_detection_table = tables[(order.index(N_date) * 2)-8]
-N_identification_table = tables[(order.index(N_date) * 2 + 1)-8]
+DR_detection_table = tables[(order.index(DR_date) * 2) - 8]
+DR_identification_table = tables[(order.index(DR_date) * 2 + 1) - 8]
+DP_detection_table = tables[(order.index(DP_date) * 2) - 8]
+DP_identification_table = tables[(order.index(DP_date) * 2 + 1) - 8]
+N_detection_table = tables[(order.index(N_date) * 2) - 8]
+N_identification_table = tables[(order.index(N_date) * 2 + 1) - 8]
 summary_detection_table = tables[-2]
 summary_identification_table = tables[-1]
 
@@ -103,7 +143,6 @@ summary_identification_table = tables[-1]
 formatted_tables = get_formatted_data(DR_excel)
 formatted_tables.extend(get_formatted_data(DP_excel))
 formatted_tables.extend(get_formatted_data(N_excel))
-
 
 N_sum = 0
 em_sum = 0
@@ -133,11 +172,13 @@ summary_identification = [[Nid_sum, Kok_sum, r, rejected_sum]]
 # pasting the tables
 paste_tables(formatted_tables[0], DR_detection_table)
 paste_tables(formatted_tables[1], DR_identification_table)
-paste_tables(formatted_tables[2], N_detection_table)
-paste_tables(formatted_tables[3], N_identification_table)
-paste_tables(formatted_tables[4], DP_detection_table)
-paste_tables(formatted_tables[5], DP_identification_table)
+paste_tables(formatted_tables[2], DP_detection_table)
+paste_tables(formatted_tables[3], DP_identification_table)
+paste_tables(formatted_tables[4], N_detection_table)
+paste_tables(formatted_tables[5], N_identification_table)
 paste_tables(summary_detection, summary_detection_table, True)
 paste_tables(summary_identification, summary_identification_table, True)
+
+format_document(doc)
 
 doc.save(output)
